8d6cb80ba607f7171c3492fde98ea49627c4d05d
Slasho01 <josuerendon01@gmail.com>
2023-12-02 00:13:31 -0300

    Finish BackEnd

--- api/src/controllers/getDogByName.js
+++ api/src/controllers/getDogByName.js
@@ -1,15 +1,30 @@
 require('dotenv').config();
 const { Dog, Temperament } = require('../db')
 const axios = require("axios");
+const { Op } = require('sequelize');
 const URLs = 'https://api.thedogapi.com/v1/breeds/search?q='
 const getDogsByName = async (req, res) => {
     const {name} = req.query
     try {
-        console.log(name)
         const respuesta = await axios.get(`${URLs}${name}`)
         const data = respuesta.data;
-        if(data.length > 0){
-            res.status(201).json(data)
+
+        const respuestadb = await Dog.findAll({
+            include: [{ model: Temperament, attributes: ['name'] }],
+            where: {
+              name: {
+                [Op.like]: `%${name}%`,
+              },
+            },
+          });
+        const dog = respuestadb.map((dbdog) => {
+            const { id, name, image, height, weight, life_span, temperaments } = dbdog.toJSON();
+            const temperamentos = temperaments.map((temperament) => temperament.name).join(', ');
+            return { id, name, image, height, weight, life_span, temperamentos }
+        })
+        const combine = data.concat(...dog)
+        if(combine.length > 0){
+            res.status(201).json(combine)
         }else{
             res.status(404).json('No exite')
         }
